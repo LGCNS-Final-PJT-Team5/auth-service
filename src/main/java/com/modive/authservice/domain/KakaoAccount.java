934ae370e9edb6b09ea12ec18702c390c1c11715
ShinYEB <dpqls9799@naver.com>
2025-05-23 14:51:42 +0900

    [chore] erd 구조 변경
    gender, birthday, phone 삭제

--- src/main/java/com/modive/authservice/domain/KakaoAccount.java
+++ src/main/java/com/modive/authservice/domain/KakaoAccount.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoAccount(
-        KakaoUserProfile profile
+        KakaoUserProfile profile,
+        String email
 ) {
 }
